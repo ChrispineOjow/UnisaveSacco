2066f873672cbbf3fcb3a538c96830b30501888f
Chrispine Ojow <WameyoChrispine@gmail.com>
2025-09-08 21:35:47 +0300

    updates

--- signUpPage/SignUp.js
+++ signUpPage/SignUp.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
             formData.append("Email", email);
             formData.append("Password", password);
 
-            fetch("../php/Models/MemberCredentials.php", {
+            fetch("#phpfile", {
                 method: "POST",
                 body: formData
             })
